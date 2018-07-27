7c5ed7fd19f378f1361c747c045d1b75747fb5f1
Yakup Güvenç <yakup@asebilisim.com>
2018-07-27 18:49:01 +0300

    sales reports vuejs edit

--- app/Http/Controllers/Modules/Sales/SalesReportController.php
+++ app/Http/Controllers/Modules/Sales/SalesReportController.php
@@ -29,6 +29,7 @@ class SalesReportController extends Controller
         $sales_orders = SalesOrders::where("account_id",aid())->get();
 
         $product_dont_category = 0;
+
         foreach($products as $product){
 
 
@@ -43,5 +44,72 @@ class SalesReportController extends Controller
             compact(
                 "kategoriler","tags","companies","company_tags",'product_dont_category','products',"sales_orders"
             ));
+    }
+
+
+    public function pies_data()
+    {
+
+        $tags = Tags::where("type", "sales_orders")->get();
+        $company_tags = Tags::where("type","companies")->where("type","companies")->get();
+        $categories = Category::where("account_id",aid())->get();
+
+
+        $labels_tag = array();
+        $data_tag = array();
+        $backgroundColor_tag = array();
+        foreach ($tags as $tag) {
+            if ($tag->sales_orders_amount != "0,00") {
+                array_push($labels_tag, $tag->title);
+                array_push($data_tag, money_db_format($tag->sales_orders_amount));
+                array_push($backgroundColor_tag, $tag->bg_color);
+            }
+        }
+
+        $dataset["orders"]["labels"] = $labels_tag;
+        $dataset["orders"]["data"] = $data_tag;
+        $dataset["orders"]["bgcolor"] = $backgroundColor_tag;
+
+
+        $labels_tag_customer = array();
+        $data_tag_customer = array();
+        $backgroundColor_tag_customer = array();
+        foreach($company_tags as $company_tag)
+        {
+            array_push($labels_tag_customer, $company_tag->title);
+            array_push($data_tag_customer, money_db_format($company_tag->companies_amount));
+            array_push($backgroundColor_tag_customer, $company_tag->bg_color);
+
+
+        }
+
+        $dataset["customers"]["labels"] = $labels_tag_customer;
+        $dataset["customers"]["data"] = $data_tag_customer;
+        $dataset["customers"]["bgcolor"] = $backgroundColor_tag_customer;
+
+
+        $labels_tag_product = array();
+        $data_tag_product = array();
+        $backgroundColor_tag_product = array();
+        foreach($categories as $category)
+        {
+            if($category->totalOrder != "0"){
+            array_push($labels_tag_product, $category->name);
+            array_push($data_tag_product, money_db_format($category->totalOrder));
+            array_push($backgroundColor_tag_product, $category->color);
+            }
+
+        }
+
+        $dataset["products"]["labels"] = $labels_tag_product;
+        $dataset["products"]["data"] = $data_tag_product;
+        $dataset["products"]["bgcolor"] = $backgroundColor_tag_product;
+
+
+
+
+        return $dataset;
+
+
     }
 }
