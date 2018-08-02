6309da41daf8ebf27604a50834c72c9c6c9544c3
Yakup Güvenç <yakup@asebilisim.com>
2018-08-02 12:22:09 +0300

    sales report edit

--- resources/views/modules/sales/sales_reports/index.blade.php
+++ resources/views/modules/sales/sales_reports/index.blade.php
@@ -40,7 +40,7 @@
                                                    v-bind:style="{'background-color':item.bgcolor}">
                                                 @{{ item.labels}}
                                                     </label>
-                                            <label class="pull-right"><b>@{{item.data}}</b> <i
+                                            <label class="pull-right"><b>@{{formatPrice(item.data)}}</b> <i
                                                         class="fa fa-try"></i></label>
                                                 </div>
 
@@ -55,7 +55,7 @@
                                                    v-bind:style="{'background-color':item.bgcolor}">
                                                 @{{ item.labels }}
                                                     </label>
-                                            <label class="pull-right"><b>@{{ item.data }}</b> <i class="fa fa-try"></i></label>
+                                            <label class="pull-right"><b>@{{formatPrice(item.data)}}</b> <i class="fa fa-try"></i></label>
                                                 </div>
 
                                     </div>
@@ -67,7 +67,7 @@
                                                            v-bind:style="{'background-color':item.bgcolor}">
                                                         @{{ item.labels }}
                                                     </label>
-                                                    <label class="pull-right"><b>@{{ item.data }}</b> <i class="fa fa-try"></i></label>
+                                                    <label class="pull-right"><b>@{{formatPrice(item.data)}}</b> <i class="fa fa-try"></i></label>
                                                 </div>
 
 
@@ -231,6 +231,12 @@
                         updateData:function(){
                           this.pies_data(this.start,this.end);
 
+                        },
+                        formatPrice($value){
+                          return $value.toLocaleString('tr-TR', {
+                              minimumFractionDigits: 2,
+                              maximumFractionDigits: 2
+                          });
                         },
                         pies_data: function (start,end) {
                             axios.post("{{route("sales.pies.data",aid())}}",{vat:this.vat,start:start,end:end}).then(res => {
