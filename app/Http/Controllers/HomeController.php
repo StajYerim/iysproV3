05e06be713a8311d847bd4e0d2670632f6d3b4f0
Ümit UZ <umituz998@gmail.com>
2018-09-12 22:38:01 +0300

    kullanici-profil

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 use App\Model\Finance\Cheques;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -244,16 +245,34 @@ class HomeController extends Controller
     }
 
     /*User profile update */
-    public function profil_update_save(Request $request){
-      $user = Auth::user();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->mobile = $request->mobile;
-      $user->save();
+    public function profil_update_save(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'max:100',
+            'email' => 'max:100',
+            'mobile' => 'max:100'
+        ]);
 
-      return response()->json([
-        "message" => "Successfully Updated!"
-      ]);
+        if ($validator->fails()) {
+            return ['title' => trans('word.error'),'message' => trans('sentence.please_check_form_again'),'type' => 'danger'];
+        }else{
+            $user = Auth::user();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->mobile = $request->mobile;
+            $user->save();
+
+            return response()->json(
+                [
+                    "title" => trans('word.success'),
+                    "message"=> trans('sentence.all_your_informations_saved'),
+                    "type" => "success"
+                ]
+            );
+        }
+
+
+
     }
 
     /* User password change save */
--- app/Http/Controllers/Modules/Settings/ApiConnectController.php
+++ app/Http/Controllers/Modules/Settings/ApiConnectController.php
@@ -19,33 +19,33 @@ class ApiConnectController extends Controller
         if($request->type == "n11")
         {
             $validator = Validator::make($request->all(), [
-                'api_key' => 'required|max:100',
-                'api_password' => 'required|max:100',
+                'api_key' => 'max:100',
+                'api_password' => 'max:100',
             ]);
 
             if ($validator->fails()) {
-                return ['title' => 'Hata','message' => 'Formu Kontrol Ediniz.','type' => 'danger'];
+                return ['title' => trans('word.error'),'message' => trans('sentence.please_check_form_again'),'type' => 'danger'];
             }else{
                 Account::find(aid())->update([
                     "n11_api_key"=>$request->api_key,
                     "n11_api_password"=>$request->api_password
                 ]);
-                return ["status" => "Success","message"=>"n11 bilgileri kayıt edildi","type" => "success"];
+                return ["title" => trans('word.success'),"message"=> trans('sentence.all_your_informations_saved'),"type" => "success"];
             }
         }
         elseif($request->type == "parasut")
         {
             $validator = Validator::make($request->all(), [
-                'client_id' => 'required|max:100',
-                'client_secret' => 'required|max:100',
-                'username' => 'required|max:100',
-                'password' => 'required|max:100',
-                'company_id' => 'required|max:100',
-                'callback_url' => 'required|max:100',
+                'client_id' => 'max:100',
+                'client_secret' => 'max:100',
+                'username' => 'max:100',
+                'password' => 'max:100',
+                'company_id' => 'max:100',
+                'callback_url' => 'max:100',
             ]);
 
             if ($validator->fails()) {
-                return ["status" => "Error","message"=>"Formu kontrol edin","type" => "danger"];
+                return ['title' => trans('word.error'),'message' => trans('sentence.please_check_form_again'),'type' => 'danger'];
             }else{
                 Account::find(aid())->update([
                     "parasut_client_id"=>$request->client_id,
@@ -55,7 +55,7 @@ class ApiConnectController extends Controller
                     "parasut_company_id"=>$request->company_id,
                     "parasut_callback_url"=>$request->callback_url,
                 ]);
-                return ["status" => "Success","message"=>"Paraşüt bilgileri kayıt edildi","type" => "success"];
+                return ["title" => trans('word.success'),"message"=> trans('sentence.all_your_informations_saved'),"type" => "success"];
             }
 
 
